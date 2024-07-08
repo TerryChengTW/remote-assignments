173c64bbf2f4da8223f28a22f2a168389eb8ff8a
TerryChengTW <terrydev.tw@gmail.com>
2024-07-08 18:00:18 +0800

    w4a1 submit

--- Week-4/Assignment-1/script.js
+++ Week-4/Assignment-1/script.js
@@ -3,15 +3,17 @@ function delayedResult(n1, n2, delayTime, callback) {
     // your code here
     setTimeout(() => {
         callback(n1 + n2);
-    
     }, delayTime);
 }
-delayedResult(4, 5, 3000, function (result) {
-    console.log("callback: " + result);
+delayedResult(4, 5, 3000, function (result1) {
+    console.log(result1)
+    // console.log("callback: " + result1);
 }); // 9 (4+5) will be shown in the console after 3 seconds
-delayedResult(-5, 10, 2000, function (result) { 
-    console.log("callback: " + result);
+delayedResult(-5, 10, 2000, function (result2) { 
+    console.log(result2)
+    // console.log("callback: " + result2);
 }); // 5 (-5+10) will be shown in the console after 2 seconds
+
 
 
 // promise
@@ -24,7 +26,8 @@ function delayedResultPromise(n1, n2, delayTime) {
     });
 }
 delayedResultPromise(4, 5, 3000).then(result => {
-    console.log("'promise': " + result);
+    console.log(result)
+    // console.log("'promise': " + result);
 });
 // 9 (4+5) will be shown in the console after 3 seconds
 
@@ -34,8 +37,10 @@ delayedResultPromise(4, 5, 3000).then(result => {
 async function main() {
         // your code here, you should call delayedResultPromise here and get the result using async/await.
         result1 = await delayedResultPromise(4, 5, 3000);
-        result2 = await delayedResultPromise(-5, 10, 3000);
-        console.log("'await': " + result1);
-        console.log("'await': " + result2);
+        result2 = await delayedResultPromise(-5, 10, 5000);
+        console.log(result1)
+        console.log(result2)
+        // console.log("'await': " + result1);
+        // console.log("'await': " + result2);
     }
     main(); // result will be shown in the console after <delayTime> seconds
